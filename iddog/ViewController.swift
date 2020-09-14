02ba683179bc8532499d027f04fdfd455e89485e
Arthur Kazuo <arthur.kazuo@hotmail.com>
2020-09-14 11:13:40 -0300

    Initial Commit

---
+++ iddog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iddog
+//
+//  Created by Arthur Kazuo on 14/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
