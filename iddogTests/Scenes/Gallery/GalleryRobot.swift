3958038d861485756b39f621add80a59fc903ae7
Arthur Kazuo <arthur.kazuo@hotmail.com>
2020-09-24 16:43:16 -0300

    Adds home view tests

---
+++ iddogTests/Scenes/Gallery/GalleryRobot.swift
@@ -0,0 +1,8 @@
+//
+//  GalleryRobot.swift
+//  iddogTests
+//
+//  Created by Arthur Kazuo on 24/09/20.
+//
+
+import Foundation
---
+++ iddogTests/Scenes/Home/HomeTests.swift
@@ -0,0 +1,45 @@
+//
+//  HomeTests.swift
+//  iddogTests
+//
+//  Created by Arthur Kazuo on 24/09/20.
+//
+
+import class KIF.KIFTestCase
+import KeychainSwift
+@testable import iddog
+
+
+final class HomeTests: KIFTestCase {
+    private var robot: HomeRobot!
+    private let keychain = KeychainSwift()
+
+    // MARK: Override functions
+
+    override func setUp() {
+        super.setUp()
+        keychain.set("1234567890", forKey: "token")
+        robot = HomeRobot(spec: self)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        keychain.clear()
+        robot = nil
+    }
+
+    func testInit() throws {
+        try robot.require()
+            .loadScreen()
+            .result()
+            .hasElements()
+    }
+
+    func testPresentGallery() throws {
+        try robot.require()
+            .loadScreen()
+            .tapCell(row: 1)
+            .result()
+            .hasGalleryView()
+    }
+}
